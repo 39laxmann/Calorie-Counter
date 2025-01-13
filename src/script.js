d8e07b262a33676c0fb124140bc0f0313fa55f6c
39laxmann <thapamagarlaxman72@gmail.com>
2025-01-13 22:02:08 +0545

    addEntry function to add the entries

--- src/script.js
+++ src/script.js
@@ -4,3 +4,31 @@ const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+
+
+
+let isError = false;
+
+
+// To match specific characters in a string, we can use Regular Expressions or "regex" for short.
+// Regex in JavaScript is indicated by a pattern wrapped in forward slashes.
+function cleanInputString(str) {
+    const regex = /[+-\s]/g;
+    return str.replace(regex, "");  //replaces any +,- or whitespaces with a empty string
+}
+
+console.log(cleanInputString("++- 21"));
+
+function isInvalidInput(str){
+    const regex =/\d+e\d+/i;
+    return str.match(regex);   //
+
+
+}
+function addEntry(){
+  //adding "#" at beginning kineki we have the access through and id
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+
+}
+
+console.log(entryDropdown.value);
